473442d6d80e70a86a4a2b2434cbf95ff042f99f
Gabriele Sales <gbrsales@gmail.com>
2023-12-21 10:09:37 +0100

    Update calls to TCGAbiolinks

--- R/dataset.R
+++ R/dataset.R
@@ -15,28 +15,24 @@ fetch_tcga_dataset <- function(disease) {
   if (file.exists(prepared_path))
     return(readRDS(prepared_path))
 
-  if (dir.exists(dest_dir))
-    unlink(dest_dir, recursive = TRUE)
-
-  se <- harmonize_datasets(
-          list(gdc_download(disease, dest_dir, "results"),
-               gdc_download(disease, dest_dir, "normalized_results")),
-          c("raw_count", "normalized_count")
-        )
+  dset <- gdc_download(disease, dest_dir)
+  se <- select_assays(
+    dset,
+    c("unstranded", "tpm_unstrand"),
+    c("raw_count", "normalized_count")
+  )
   saveRDS(se, file = prepared_path)
 
   return(se)
 }
 
-gdc_download <- function(disease, dest_dir, file_type) {
+gdc_download <- function(disease, dest_dir) {
   query <- GDCquery(
     project = glue("TCGA-", disease),
-    data.category = "Gene expression",
-    data.type = "Gene expression quantification",
-    platform = "Illumina HiSeq",
-    experimental.strategy = "RNA-Seq",
-    sample.type = "Primary Tumor",
-    file.type = file_type
+    data.category = "Transcriptome Profiling",
+    data.type = "Gene Expression Quantification",
+    workflow.type = "STAR - Counts",
+    sample.type = "Primary Tumor"
   )
   if (is.null(query))
     abort(glue("no gene expression data for disease: {disease}"))
@@ -53,57 +49,11 @@ remove_duplicates <- function(query) {
   return(query)
 }
 
-harmonize_datasets <- function(datasets, assays) {
-  samples <- sort(colnames(datasets[[1]]))
-  meta <- colnames(colData(datasets[[1]]))
-  template <- NULL
-  selected_assays <- list()
-
-  for (i in seq_along(datasets)) {
-    dataset <- datasets[[i]]
-    dataset <- dataset[, samples]
-    colData(dataset) <- colData(dataset)[samples, meta]
-    dataset <- simplify_rowdata(dataset)
-
-    if (i == 1) {
-      template <- dataset
-    }
-
-    if (!identical(colnames(dataset), samples)) {
-      abort("mismatch in sample names")
-    } else if (!identical(rowData(dataset), rowData(template))) {
-      abort("mismatch in rowData")
-    }
-
-    dataset_cdata <- colData(dataset)
-    template_cdata <- colData(template)
-    if (!identical(rownames(dataset_cdata), rownames(template_cdata))) {
-      abort("mismatch in colData row names")
-    } else if (!identical(colnames(dataset_cdata), colnames(template_cdata))) {
-      abort("mismatch in colData column names")
-    }
-
-    found_assays <- assayNames(dataset)
-    common_assays <- intersect(found_assays, assays)
-    if (length(common_assays) == 0) {
-      abort("dataset with no usable assay")
-    }
-
-    for (assay_name in common_assays) {
-      selected_assays[[assay_name]] <- assay(dataset, common_assays)
-    }
-  }
-
-  assays(template) <- selected_assays
-  template
-}
-
-simplify_rowdata <- function(se) {
-  dat <- rowData(se)
-  dat <- dat[, c("gene_id", "entrezgene", "ensembl_gene_id")]
-  rowData(se) <- dat
-  rownames(se) <- dat[, "gene_id"]
-  se
+select_assays <- function(dataset, selection, names_) {
+  assays_ <- lapply(selection, \(n) assay(dataset, n))
+  names(assays_) <- names_
+  assays(dataset) <- assays_
+  return(dataset)
 }
 
 
